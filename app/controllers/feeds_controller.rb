8ce0e5bc0623a841158088879d5b346831673928
Loïc Schülé <lschule@gmail.com>
2012-03-15 19:50:30 +0100

    FAUT APPRENDRE A ECRIRE JEAN LUC!! fixing his typos!!

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -6,7 +6,7 @@ class FeedsController < ApplicationController
 
     respond_to do |format|
       format.html # index.html.erb
-      format.json { render json: @feeds }
+      format.json { render :json => @feeds }
     end
   end
 
@@ -17,7 +17,7 @@ class FeedsController < ApplicationController
 
     respond_to do |format|
       format.html # show.html.erb
-      format.json { render json: @feed }
+      format.json { render :json => @feed }
     end
   end
 
@@ -28,7 +28,7 @@ class FeedsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @feed }
+      format.json { render :json => @feed }
     end
   end
 
@@ -44,11 +44,11 @@ class FeedsController < ApplicationController
 
     respond_to do |format|
       if @feed.save
-        format.html { redirect_to @feed, notice: 'Feed was successfully created.' }
-        format.json { render json: @feed, status: :created, location: @feed }
+        format.html { redirect_to @feed, :notice => 'Feed was successfully created.' }
+        format.json { render :json => @feed, :status => :created, :location => @feed }
       else
-        format.html { render action: "new" }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.json { render :json => @feed.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -60,11 +60,11 @@ class FeedsController < ApplicationController
 
     respond_to do |format|
       if @feed.update_attributes(params[:feed])
-        format.html { redirect_to @feed, notice: 'Feed was successfully updated.' }
+        format.html { redirect_to @feed, :notice => 'Feed was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: "edit" }
-        format.json { render json: @feed.errors, status: :unprocessable_entity }
+        format.html { render :action => "edit" }
+        format.json { render :json => @feed.errors, :status => :unprocessable_entity }
       end
     end
   end
