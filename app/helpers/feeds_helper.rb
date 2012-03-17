b81cc8deed7e4cf7bccaf5b064dfd0db5443eb2c
Loïc Schülé <lschule@gmail.com>
2012-03-17 22:44:31 +0100

    improve feeds add/remove ajaxy + article page if no article with link to feeds

--- app/helpers/feeds_helper.rb
+++ app/helpers/feeds_helper.rb
@@ -1,2 +1,13 @@
 module FeedsHelper
+  def check_if_disabled(disabled_if,current_user,feed)
+    if current_user.feeds.include?(feed)
+      if !disabled_if
+        'disabled'
+      end
+    else
+      if disabled_if 
+        'disabled'
+      end
+    end
+  end
 end
