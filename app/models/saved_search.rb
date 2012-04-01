258829c5253642ee4b2b442a103b68287febdc25
Jean-Luc Geering <jlgeering.12@gmail.com>
2012-04-01 18:18:19 +0200

    saved searches only with user's feeds

--- app/models/saved_search.rb
+++ app/models/saved_search.rb
@@ -2,12 +2,12 @@ class SavedSearch < ActiveRecord::Base
   belongs_to :user
 
   def articles
-    search_params = {:query => query, :after => last_access}
+    search_params = {:query => query, :after => last_access, :feeds => user.feeds.collect { |item| item.id }}
     Article.search(search_params)
   end
 
   def count
-    search_params = {:query => query, :after => last_access}
+    search_params = {:query => query, :after => last_access, :feeds => user.feeds.collect { |item| item.id }}
     Article.count(search_params)
   end
 
