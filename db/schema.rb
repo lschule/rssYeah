13d035606d6e87fc06718757a444d520dea3fbb0
Gabriel Leventhal <gabriel@leventhal.ch>
2012-06-07 20:45:23 +0200

    schema for migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120401154108) do
+ActiveRecord::Schema.define(:version => 20120607184123) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(:version => 20120401154108) do
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
     t.integer  "user_id"
+    t.string   "name"
   end
 
   create_table "smart_categories", :force => true do |t|
