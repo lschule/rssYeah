b51c481a6d0268b06b72793ed3dd98c923427b38
Gabriel Leventhal <gabriel@leventhal.ch>
2012-07-04 14:53:50 +0200

    Added capistrano config for ETH server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,18 @@
 require 'bundler/capistrano'
 
-set :user, 'gabriel'
 set :use_sudo, false
-set :domain, 'uwis-cx-dock-11-305.ethz.ch'
-set :applicationdir, '/Users/gabriel/Projects/rssYeah-prod'
 
-set :application, 'rssYeah'
-set :scm, 'git'
-set :scm_verbose, true
-set :repository, 'git://github.com/lschule/rssYeah.git'
-#set :repository, "ssh://#{user}@#{domain}/Users/gabriel/Projects/rssYeah"
-set :branch, 'master'
 set :git_shallow_clone, 1
+set :keep_releases,     5
+set :application,       'rssYeah'
+set :user,              'rssyeah'
+set :domain,            'uwis-cx-dock-13-225.ethz.ch'
+set :applicationdir,    '/Users/rssyeah/rssyeah'
+set :runner,            'rssyeah'
+set :scm,               'git'
+set :scm_verbose,       true
+set :repository,        'git://github.com/lschule/rssYeah.git'
+set :branch,            'master'
 
 # roles (servers)
 role :web, domain
@@ -23,6 +24,9 @@ set :deploy_to, applicationdir
 set :deploy_via, :export
 
 set :bundle_cmd, '/opt/local/bin/bundle'
+
+ssh_options[:paranoid]    = false
+default_run_options[:pty] = true
 
 # Passenger
 namespace :deploy do
