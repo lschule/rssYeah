c7ce0a74396ef3c9373ca5581d2793b1855e513d
Gabriel Leventhal <gabriel@leventhal.ch>
2012-12-05 09:41:00 +0100

    Added option to turn off article fetching on some feeds.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120612205843) do
+ActiveRecord::Schema.define(:version => 20121205081356) do
 
   create_table "articles", :force => true do |t|
     t.string   "name"
@@ -29,8 +29,9 @@ ActiveRecord::Schema.define(:version => 20120612205843) do
     t.string   "name"
     t.string   "shortname"
     t.string   "url"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                   :null => false
+    t.datetime "updated_at",                   :null => false
+    t.boolean  "fetch",      :default => true
   end
 
   create_table "feeds_users", :id => false, :force => true do |t|
